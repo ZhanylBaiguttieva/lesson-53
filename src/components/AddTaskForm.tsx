6676b89ea336cbde482457ca91087c5260b6dd34
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-06 02:06:13 +0600

    Added style in AddTaskDorm.tsx

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -10,9 +10,18 @@ const AddTaskForm: React.FC<Props> = ({
                                         onTaskChange
                                       }) => {
   return (
-    <div className="taskForm">
-      <input onChange={onTaskChange} id='inputId' />
-      <button onClick={onAdd}>Add task</button>
+    <div className='row m-1 g-3'>
+      <div className='col-auto'>
+        <input type='text'
+               placeholder="Add new task"
+               className="form-control"
+               onChange={onTaskChange}
+        />
+      </div>
+      <div className='col-auto'>
+        <button className="btn btn-primary"
+                onClick={onAdd}>Add task</button>
+      </div>
     </div>
   );
 };
