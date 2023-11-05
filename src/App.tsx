9724cde2fff787ffa39cd948b453bc512c569513
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-06 02:07:10 +0600

    Added function changeTask, deleteTask, addTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from "react";
+import Task from "./components/Task";
+import AddTaskForm from "./components/AddTaskForm";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ExampleOfTask  {
+  id: string;
+  info: string;
 }
 
-export default App
+function App() {
+  const [tasks, setTasks] = useState <ExampleOfTask[]> ([
+    {info: 'Make dinner', id: '1'},
+    {info: 'Take shower', id: '2'},
+    {info: 'Clean the bathroom', id: '3'},
+  ]);
+  const [currentTask, setCurrentTask] = useState('');
+
+  const changeTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentTask(event.target.value);
+  };
+
+  const deleteTask = (id:string) => {
+    setTasks((prevState ) => prevState.filter(task => {
+      return task.id !== id;
+    }));
+  };
+
+  const addTask = () => {
+    setTasks(prevState => {
+      const lastItem = prevState[prevState.length - 1];
+      const idNumber = lastItem ? lastItem.id + 1 : 1;
+      const newId = String(idNumber);
+      // const newTask = {info: currentTask, newId};
+      return [...prevState,{
+        id: newId,
+        info: currentTask,
+      }];
+    });
+  };
+
+  return (
+    <div className='container mt-2'>
+      {tasks.length !== 0 &&
+        tasks.map(task =>
+          <Task
+            info={task.info}
+            onDelete={() => deleteTask(task.id)}
+          >
+          </Task>)}
+      <AddTaskForm
+        onTaskChange = {(event) => changeTask(event)}
+        onAdd = {() => addTask()}
+      >
+      </AddTaskForm>
+    </div>
+  );
+}
+export default App;
